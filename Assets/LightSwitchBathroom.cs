c7221aefab0844ef0f523fdf98ec4d526e688545
alecheadrick <alecheadrick@gmail.com>
2018-03-20 21:09:58 -0400

    added button puzzle and door to the bathroom

---
+++ Assets/LightSwitchBathroom.cs
@@ -0,0 +1,56 @@
+﻿namespace VRTK.Examples
+{
+	using UnityEngine;
+	using UnityEventHelper;
+
+	public class LightSwitchBathroom : MonoBehaviour
+	{
+		public GameObject wallText;
+		public GameObject[] lightBulb;
+		public GameObject go;
+		public Transform dispenseLocation;
+		public bool lightOn;
+
+		private VRTK_Button_UnityEvents buttonEvents;
+
+		private void Start()
+		{
+			buttonEvents = GetComponent<VRTK_Button_UnityEvents>();
+			if (buttonEvents == null)
+			{
+				buttonEvents = gameObject.AddComponent<VRTK_Button_UnityEvents>();
+			}
+			buttonEvents.OnPushed.AddListener(handlePush);
+			wallText.SetActive(false);
+		}
+		public void TurnOffLightSwitch()
+		{
+			for (int i = 0; i < lightBulb.Length; i++) {
+				lightBulb[i].SetActive(false);
+			}
+			wallText.SetActive(true);
+			lightOn = false;
+		}
+		public void TurnOnLightSwitch()
+		{
+			for (int i = 0; i < lightBulb.Length; i++)
+			{
+				lightBulb[i].SetActive(true);
+			}
+			wallText.SetActive(true);
+			lightOn = true;
+		}
+
+		private void handlePush(object sender, Control3DEventArgs e)
+		{
+			VRTK_Logger.Info("Pushed");
+			if (lightOn)
+			{
+				TurnOffLightSwitch();
+			}
+			else {
+				TurnOnLightSwitch();
+			}
+		}
+	}
+}
