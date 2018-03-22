1a8ca95ce2db363073d0cd71a58ade88c8f424e3
alecheadrick <alecheadrick@gmail.com>
2018-03-22 18:19:55 -0400

    added a fix dial script

---
+++ Assets/FixDials.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixDials : MonoBehaviour {
+	#region Variables
+	
+	#endregion
+	
+	#region Methods
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+	
+	#endregion
+}
