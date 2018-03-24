6d1c5eddf6677ee008428431255cb3f7470ce358
alecheadrick <alecheadrick@gmail.com>
2018-03-24 14:36:47 -0400

    added camera follow and icons

---
+++ Assets/UiFaceCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiFaceCamera : MonoBehaviour {
+	#region Variables
+	public GameObject mCam;
+	#endregion
+	
+	#region Methods
+	void Start () {
+		mCam = GameObject.FindGameObjectWithTag("MainCamera");
+	}
+	
+	void Update () {
+		if (mCam == null) {
+			mCam = GameObject.FindGameObjectWithTag("MainCamera");
+		}
+		transform.LookAt(mCam.transform.position);
+	}
+	
+	#endregion
+}
