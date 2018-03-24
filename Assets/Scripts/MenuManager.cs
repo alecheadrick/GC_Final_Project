f64566c723efcc40e34778acde952f07ca073862
Emily Nelson <xcookie623x@gmail.com>
2018-03-24 14:35:07 -0400

    menu scene

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+	public static bool playMusic = true;
+	public static string level = "Master Level";
+
+	public Button begin;
+
+	public void Begin()
+	{
+		SceneManager.LoadScene(level);
+	}
+}
