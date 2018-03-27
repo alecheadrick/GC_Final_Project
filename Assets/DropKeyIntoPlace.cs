e93877214381d52cdfb530fb2a9cfd52f4bdb3dd
alecheadrick <alecheadrick@gmail.com>
2018-03-27 15:41:41 -0400

    added key and key interactions

---
+++ Assets/DropKeyIntoPlace.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+using VRTK.Examples;
+
+public class DropKeyIntoPlace : MonoBehaviour {
+
+	#region Singelton
+	public static DropKeyIntoPlace instance
+	{
+		get
+		{
+			if (_instance == null)
+			{
+				_instance = FindObjectOfType<DropKeyIntoPlace>();
+			}
+			return _instance;
+		}
+	}
+	static DropKeyIntoPlace _instance;
+
+	void Awake()
+	{
+		_instance = this;
+	}
+	#endregion
+
+	#region Variables
+	public GameObject key;
+	public Transform dropPlace;
+	public GameObject door;
+	public GameObject endTrigger;
+	public bool keyInDropZone = false;
+	#endregion
+
+	#region Methods
+	public void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.name == key.name) {
+			keyInDropZone = true;
+		}
+	}
+	public void OnTriggerExit(Collider other)
+	{
+		if (other.gameObject.name == key.name)
+		{
+			keyInDropZone = false;
+		}
+	}
+	public void DropIntoPlace() {
+			key.transform.position = dropPlace.position;
+			key.GetComponent<KeyIO>().enabled = false;
+			key.GetComponent<Collider>().enabled = false;
+			door.GetComponent<Openable_Door>().enabled = true;
+			endTrigger.SetActive(true);
+	}
+
+	#endregion
+}
---
+++ Assets/KeyIO.cs
@@ -0,0 +1,25 @@
+﻿namespace VRTK.Examples
+{
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+
+	[System.Serializable]
+	public class KeyIO : VRTK_InteractableObject
+	{
+		#region Methods
+		public override void Ungrabbed(VRTK_InteractGrab previousGrabbingObject)
+		{
+			if (DropKeyIntoPlace.instance.keyInDropZone == true)
+			{
+				DropKeyIntoPlace.instance.DropIntoPlace();
+			}
+			else if (DropKeyIntoPlace.instance.keyInDropZone == false) {
+				base.Ungrabbed(previousGrabbingObject);
+			}
+			
+		}
+
+		#endregion
+	}
+}
